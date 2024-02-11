f4596bad3f9a77f5f6b1b53c08172e8cb5a1ee7d
volcanodoryshrimpnettuba <111703011+volcanodoryshrimpnettuba@users.noreply.github.com>
2024-02-11 02:27:25 +0100

    del

--- 08_Web_Network/hidden.py
+++
@@ -1,9 +0,0 @@
-# Keep this file separate
-
-# Twitter developer keys
-
-def oauth():
-    return {"consumer_key": "UyGQBEDwMiMGsIXUZPH0tiBaw",
-            "consumer_secret": "O9jkpP32dsMTHGwWPSlBKGht0lT2AnlD144TIeL84hJSHByWWi",
-            "token_key": "1556363913943474183-z63iGyTNhIvGCuoayVX2m4QQBdjjfa",
-            "token_secret": "QHkX5PnkBplkSGZdVsBz0Ho15btBPLBd7GaA1eWuwv8o1"}
--- 20_Javascript/02 React Test/react_test/src/components/Context1/LevelContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from 'react';
-
-export const LevelContext = createContext(0);   // 0 is the default value
